e6c77e6f183879b5c6c6bdfac6b46efec2ac2513
Jorge Reyes <reyestrevjorge@gmail.com>
2022-06-18 23:47:40 -0500

    Display all the tasks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,49 @@
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { Box, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 import ToDo from '../components/ToDo/ToDo';
 import Main from '../layouts/Main/Main';
 
 const Index = () => {
+   const [tasks, setTasks] = useState([1, 2, 3, 4]);
+   const [isCreateMode, setIsCreateMode] = useState(false);
+
+   const onCreateNewTodoClick = () => {
+      setIsCreateMode(true);
+   };
+
+   // Create New To-Do button
+   const createNewTodo = (
+      <Button
+         size="lg"
+         rightIcon={<PlusSquareIcon />}
+         onClick={onCreateNewTodoClick}>
+         Create a To-Do
+      </Button>
+   );
+
+   // Create New To-Do template
+   const createNewTodoTemplate = (
+      <ToDo
+         title="To-Do Title"
+         body="Brief description of the To-Do"
+         date="2022, May 12"
+      />
+   );
+
    return (
       <Main>
-         <ToDo />
+         <Box display="flex" flexDirection="column" gap={4}>
+            {tasks.map((index, task) => (
+               <ToDo
+                  title="First To-Do"
+                  body="This is the body of my first To-Do task"
+                  date="2022, May 12"
+                  key={index}
+               />
+            ))}
+            {isCreateMode ? createNewTodoTemplate : createNewTodo}
+         </Box>
       </Main>
    );
 };
