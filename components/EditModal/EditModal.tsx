6330577d66e3a1e8be468d0e1720cebb9f561f82
Jorge Reyes <reyestrevjorge@gmail.com>
2022-06-20 11:15:26 -0500

    Update EditModal UI

--- components/EditModal/EditModal.tsx
+++ components/EditModal/EditModal.tsx
@@ -8,19 +8,25 @@ const EditModal = ({
 }) => {
    return (
       <Box
-         as="section"
          width="100%"
          height="100vh"
          position="absolute"
          top={0}
          left={0}
          p={4}
-         borderRadius={8}
-         bg="gray.50"
+         bg="blackAlpha.200"
          display="flex"
          justifyContent="center"
          flexDirection="column">
-         <Box>
+         <Box
+            bg="white"
+            width="50%"
+            p={8}
+            borderRadius={8}
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)">
             <Box display="flex" flexDirection="column" gap={3} mb={4}>
                <Input
                   placeholder="New Title"
