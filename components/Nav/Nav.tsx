b47b9f0a2fe29ef9cce4bce21c62fc15eec3becc
Jorge Reyes <reyestrevjorge@gmail.com>
2022-06-20 17:25:34 -0500

    Use localStorage to save the list name

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,14 +1,47 @@
-import { Flex, Spacer, Center, Button, Heading } from '@chakra-ui/react';
+import {
+   Flex,
+   Spacer,
+   Center,
+   Button,
+   Editable,
+   EditableInput,
+   EditablePreview,
+} from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { useColorMode } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 const Nav = () => {
    const { colorMode, toggleColorMode } = useColorMode();
+   const [listName, setListName] = useState('To-Do List');
+
+   useEffect(() => {
+      const response = localStorage.getItem('listName');
+
+      response && setListName(response);
+   }, []);
+
+   useEffect(() => {
+      localStorage.setItem('listName', listName);
+   }, [listName]);
 
    return (
       <Flex as="nav" p="2">
          <Center>
-            <Heading fontSize="lg">To-Do List</Heading>
+            <Editable
+               defaultValue={listName}
+               value={listName}
+               size="lg"
+               onChange={(newValue) => {
+                  if (newValue == '') {
+                     setListName('To-Do List');
+                  } else {
+                     setListName(newValue);
+                  }
+               }}>
+               <EditablePreview />
+               <EditableInput />
+            </Editable>
          </Center>
          <Spacer />
          <Center>
